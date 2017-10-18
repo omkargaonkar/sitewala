e8248b88d9dbd17582c23849dc42de49524a96a4
omkargaonkar <omkargaonkar14@gmail.com>
2017-10-18 13:52:03 +0530

    Added the Setting.php file

---
+++ sites/default/setting.local.php
@@ -0,0 +1,11 @@
+
+<?php
+$databases['default']['default'] = array(
+    'database' => 'sitewala',
+    'username' => 'root',
+    'password' => 'password',
+    'host' => 'localhost',
+    'driver' => 'mysql',
+    'port' => 3306,
+    'prefix' => '',
+);
